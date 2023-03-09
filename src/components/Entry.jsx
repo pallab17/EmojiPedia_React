560fac11bf1dca0499da30fbea7a9d40bab002e3
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-03-09 18:04:39 +0530

    Update Entry.jsx

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,1 +1,18 @@
 
+import React from "react";
+
+function Entry(pall) {
+  return (
+    <div className="term">
+      <dt>
+        <span className="emoji" role="img" aria-label="Tense Biceps">
+          {pall.img}
+        </span>
+        <span>{pall.name}</span>
+      </dt>
+      <dd>{pall.meaning}</dd>
+    </div>
+  );
+}
+
+export default Entry;
